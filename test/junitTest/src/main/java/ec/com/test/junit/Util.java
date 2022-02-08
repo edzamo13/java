69c8fe46caa5579daa6122645a6a4fa6c6e1a9f1
edzamo13@gmail.com <ezamora@mag.gob.ec>
2022-02-07 21:42:42 -0500

    add test..

---
+++ test/junitTest/src/main/java/ec/com/test/junit/Util.java
@@ -0,0 +1,79 @@
+package ec.com.test.junit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+
+    public void runMethods(String[] args) {
+        //Public method uses all private methods
+    }
+
+    private static int getNumber() {
+        return 1;
+    }
+
+    private List<String> getLanguages() {
+        List languages = new ArrayList<String>();
+        languages.add("Java");
+        languages.add("Sql");
+        languages.add("Javascript");
+        return languages;
+    }
+
+    private void addLanguage(List languages, String language) {
+        languages.add(language);
+    }
+
+    private ProgrammingLanguages getJavaLanguageClass() {
+        ProgrammingLanguages languages = new ProgrammingLanguages();
+        languages.setLanguageId(1);
+        languages.setLanguageName("Java");
+        languages.setNoOfEnrolments(100);
+        return languages;
+    }
+
+    private ProgrammingLanguages createLanguageClass(int languageId, String languageName, int noOfEnrolments) {
+        ProgrammingLanguages languages = new ProgrammingLanguages();
+        languages.setLanguageId(languageId);
+        languages.setLanguageName(languageName);
+        languages.setNoOfEnrolments(noOfEnrolments);
+        return languages;
+    }
+
+    private List<ProgrammingLanguages> addLanguageClass(ProgrammingLanguages language) {
+        List<ProgrammingLanguages> languageList = new ArrayList<ProgrammingLanguages>();
+        languageList.add(language);
+        return languageList;
+    }
+
+    class ProgrammingLanguages {
+        int languageId;
+        String languageName;
+        int noOfEnrolments;
+
+        public int getLanguageId() {
+            return languageId;
+        }
+
+        public void setLanguageId(int languageId) {
+            this.languageId = languageId;
+        }
+
+        public String getLanguageName() {
+            return languageName;
+        }
+
+        public void setLanguageName(String languageName) {
+            this.languageName = languageName;
+        }
+
+        public int getNoOfEnrolments() {
+            return noOfEnrolments;
+        }
+
+        public void setNoOfEnrolments(int noOfEnrolments) {
+            this.noOfEnrolments = noOfEnrolments;
+        }
+    }
+}
---
+++ test/junitTest/src/test/java/ec/com/test/junit/UtilTest.java
@@ -0,0 +1,86 @@
+package ec.com.test.junit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(PowerMockRunner.class)
+class UtilTest {
+
+    @Test
+    void test_getNumber() throws Exception {
+        int obj = Whitebox.invokeMethod(new Util(), "getNumber");
+        assertEquals(obj, 1);
+    }
+
+    @Test
+    public void test_getLanguages() throws Exception {
+        List obj = Whitebox.invokeMethod(new Util(), "getLanguages");
+        assertEquals(obj.get(0), "Java");
+    }
+
+    @Test
+    public void test_addLanguage() throws Exception {
+        List languages = new ArrayList();
+        languages.add("Java");
+        languages.add("Sql");
+        languages.add("JavaScript");
+
+        Whitebox.invokeMethod(new Util(), "addLanguage", languages, "Python");
+        assertEquals(languages.get(0), "Java");
+    }
+
+    @Test
+    public void test_getLanguageClass() throws Exception
+    {
+        Object obj =Whitebox.invokeMethod(new Util(),"getJavaLanguageClass");
+        assertEquals(obj.getClass().getDeclaredField("languageId").get(obj), 1);
+        assertEquals(obj.getClass().getDeclaredField("languageName").get(obj), "Java");
+        assertEquals(obj.getClass().getDeclaredField("noOfEnrolments").get(obj), 100);
+    }
+
+    @Test
+    public void test_createLanguageClass() throws Exception
+    {
+        Object obj =Whitebox.invokeMethod(new Util(),"createLanguageClass",2,"Sql",240);
+        assertEquals(obj.getClass().getDeclaredField("languageId").get(obj), 2);
+        assertEquals(obj.getClass().getDeclaredField("languageName").get(obj), "Sql");
+        assertEquals(obj.getClass().getDeclaredField("noOfEnrolments").get(obj), 240);
+    }
+    @Test
+    public void test_addLanguageClass() throws Exception
+    {
+        Object programmingLanguages = new Object();
+        Class clazz = Whitebox.getInnerClassType(Util.class, "ProgrammingLanguages");
+        Constructor constructor = Whitebox.getConstructor(clazz,Util.class);
+        programmingLanguages = constructor.newInstance(new Util());
+        Field field =  null;
+
+        field = programmingLanguages.getClass().getDeclaredField("languageId");
+        field.setAccessible(true);
+        field.set(programmingLanguages, 1);
+
+        field = programmingLanguages.getClass().getDeclaredField("languageName");
+        field.setAccessible(true);
+        field.set(programmingLanguages, "Java");
+
+        field = programmingLanguages.getClass().getDeclaredField("noOfEnrolments");
+        field.setAccessible(true);
+        field.set(programmingLanguages, 100);
+
+        List obj = Whitebox.invokeMethod(new Util(),"addLanguageClass",programmingLanguages);
+        assertEquals(obj.get(0).getClass().getDeclaredField("languageId").get(obj.get(0)), 1);
+        assertEquals(obj.get(0).getClass().getDeclaredField("languageName").get(obj.get(0)), "Java");
+        assertEquals(obj.get(0).getClass().getDeclaredField("noOfEnrolments").get(obj.get(0)), 100);
+    }
+
+}
