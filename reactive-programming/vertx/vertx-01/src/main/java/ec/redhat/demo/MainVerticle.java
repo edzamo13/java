a8b65a09b01da4d7074cc0f74234d6956f0fa224
EDWIN ZAMORA <ezamoram@pichincha.com>
2024-04-25 15:12:57 -0500

    add example vertx

--- reactive-programming/vertx/vertx-01/src/main/java/ec/redhat/demo/MainVerticle.java
+++ reactive-programming/vertx/vertx-01/src/main/java/ec/redhat/demo/MainVerticle.java
@@ -1,15 +1,30 @@
 package ec.redhat.demo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
+        Router router = Router.router(vertx);
+        router.get("/api/v1/hello")
+                .handler(context -> {
+                    context
+                            .request()
+                            .response()
+                            .end("Hello vert.x world!..");
+                });
+        router.get("/api/v1/hello/:name")
+                .handler(context -> {
+                    String name = context.pathParam("name");
+                    context
+                            .request()
+                            .response()
+                            .end(String.format("Hello %s!", name));
+                });
         vertx.createHttpServer()
-                .requestHandler(req -> {
-                    req.response().end("Hello Vert.x World!..");
-                })
+                .requestHandler(router)
                 .listen(8080);
     }
 
---
+++ reactive-programming/vertx/vertx-01/src/main/java/ec/redhat/demo/MainVerticle001.java001
@@ -0,0 +1,16 @@
+package ec.redhat.demo;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.createHttpServer()
+                .requestHandler(req -> {
+                    req.response().end("Hello Vert.x World!..");
+                })
+                .listen(8080);
+    }
+
+}
